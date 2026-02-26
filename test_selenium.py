b7be164c877c4a15bc604e38e41b383d0f0c046a
Misha T <mt91106@gmail.com>
2026-02-26 20:20:27 +0400

    4Fix deprecated upload-artifact v4

--- test_selenium.py
+++ test_selenium.py
@@ -1,13 +1,3 @@
-def test_open_google():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-
-    driver.get("https://google.com")
-
+def test_open_google(driver):
+    driver.get("https://www.google.com")
     assert "Google" in driver.title
-
-    driver.quit()
